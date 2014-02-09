14060c59be5de57712b587e5fbe8c779b209574f
Bill Cox <bill@triadsemi.com>
2014-02-09 15:59:42 -0500

    Rewrote constmult

--- constmult/constmult.c
+++ constmult/constmult.c
@@ -3,113 +3,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct NodeStruct *Node;
-
-struct NodeStruct {
-    Node bigPrevNode;
-    Node smallPrevNode;
-    uint32_t value;
-    uint32_t numOperations; // This is the max of numOperations of prev nodes + 1
-    bool subtract;         // True if we subtractted the smaller value, rather than add
-    bool visited;
-};
+uint32_t *values;
+uint32_t *prevValues;
+uint32_t *nodes;
+bool *visited;
+uint32_t numNodes = 1;
 
 // Create a new node.
-static Node createNode(Node *nodes, uint32_t value, Node bigPrevNode, Node smallPrevNode, bool subtract) {
+static inline void createNode(uint32_t value, uint32_t prevValue, uint32_t targetValue) {
     // Having a 0 small value indicates a free shift of a prior value
-    Node node = (struct NodeStruct *)calloc(1, sizeof(struct NodeStruct));
-    node->value = value;
-    node->bigPrevNode = bigPrevNode;
-    node->smallPrevNode = smallPrevNode;
-    uint32_t numOperations = 0;
-    if(bigPrevNode != 0) {
-        numOperations = bigPrevNode->numOperations;
-        if(smallPrevNode != 0) {
-            if(smallPrevNode->numOperations > numOperations) {
-                numOperations = smallPrevNode->numOperations;
-            }
-            numOperations++;
-        }
+    //printf("Creating node %u\n", value);
+    if(value >> 1 > targetValue) {
+        fprintf(stderr, "value too large\n");
+        exit(1);
     }
-    node->numOperations = numOperations;
-    node->subtract = subtract;
-    nodes[value] = node;
-    return node;
+    if(value == 0) {
+        fprintf(stderr, "value == 0\n");
+        exit(1);
+    }
+    if(nodes[value] != 0) {
+        fprintf(stderr, "Recomputing value %u\n", value);
+        exit(1);
+    }
+    values[numNodes] = value;
+    prevValues[numNodes] = prevValue;
+    nodes[value] = numNodes++;
 }
 
 // Add all powers of 2 times the value to the node table.
-static void addPowersOfTwo(Node *nodes, Node prevNode, uint32_t targetValue) {
-    uint32_t value = prevNode->value;
-    while(value < targetValue*2) {
-        if(nodes[value] == NULL) {
-            prevNode = createNode(nodes, value, prevNode, NULL, false);
+static inline void addPowersOfTwo(uint32_t value, uint32_t targetValue) {
+    while(true) {
+        if(value << 1 < value || value > targetValue) {
+            return; // Overflow
         }
         value <<= 1;
-    }
-}
-
-// Given all the values in the node table, add all the values we could compute with one
-// more adder or subtractor.  Basically, for each node that can be computed with exactly
-// numOperations, find all the smaller nodes computed with numOperations or less, and add
-// or subtract it.  Then, add all powers of two to the table.
-static void addOneAdderOrSubtractor(Node *nodes, uint32_t targetValue, uint32_t numOperations) {
-    uint32_t i;
-    for(i = 1; i < targetValue*2; i++) {
-        Node bigNode = nodes[i];
-        if(bigNode != NULL && bigNode->numOperations == numOperations) {
-            uint32_t j;
-            for(j = 1; j < i; j++) {
-                Node smallNode = nodes[j];
-                Node node;
-                if(smallNode != NULL && smallNode->numOperations <= numOperations) {
-                    if(i + j < 2*targetValue) {
-                        node = nodes[i + j];
-                        if(node == NULL) {
-                            node = createNode(nodes, i + j, bigNode, smallNode, false);
-                            addPowersOfTwo(nodes, node, targetValue);
-                        }
-                    }
-                    node = nodes[i - j];
-                    if(node == NULL) {
-                        node = createNode(nodes, i - j, bigNode, smallNode, false);
-                        addPowersOfTwo(nodes, node, targetValue);
-                    }
-                }
-            }
+        if(nodes[value] == 0) {
+            createNode(value, value >> 1, targetValue);
         }
     }
 }
 
 // Print how to compute the node.
-static void printNodePathRec(Node node) {
-    node->visited = true;
-    Node smallPrevNode = node->smallPrevNode;
-    Node bigPrevNode = node->bigPrevNode;
-    if(smallPrevNode != NULL && !smallPrevNode->visited) {
-        printNodePathRec(smallPrevNode);
+static void printNodePath(uint32_t value) {
+    uint32_t nodeIndex = nodes[value];
+    visited[value] = true;
+    uint32_t bigPrevValue = prevValues[nodeIndex];
+    if(bigPrevValue != 0 && !visited[bigPrevValue]) {
+        printNodePath(bigPrevValue);
     }
-    if(bigPrevNode != NULL && !bigPrevNode->visited) {
-        printNodePathRec(bigPrevNode);
-    }
-    if(bigPrevNode != NULL && smallPrevNode != NULL) {
-        if(node->subtract) {
-            printf("%u = %u - %u\n", node->value, bigPrevNode->value, smallPrevNode->value);
+    uint32_t smallPrevValue = 0;
+    if(bigPrevValue != 0 && bigPrevValue << 1 != value) {
+        if(bigPrevValue < value) {
+            smallPrevValue = value - bigPrevValue;
         } else {
-            printf("%u = %u + %u\n", node->value, bigPrevNode->value, smallPrevNode->value);
+            smallPrevValue = bigPrevValue - value;
         }
-    } else if(bigPrevNode != NULL) {
-        printf("%u = %u << 1\n", node->value, bigPrevNode->value);
+        if(smallPrevValue != 0 && !visited[smallPrevValue]) {
+            printNodePath(smallPrevValue);
+        }
+    }
+    if(bigPrevValue << 1 == value) {
+        printf("%u = %u << 1\n", value, bigPrevValue);
+    } else if(bigPrevValue != 0 && smallPrevValue != 0) {
+        if(bigPrevValue > value) {
+            printf("%u = %u - %u\n", value, bigPrevValue, smallPrevValue);
+        } else {
+            printf("%u = %u + %u\n", value, bigPrevValue, smallPrevValue);
+        }
     }
 }
 
-// Print how to compute the node.
-static void printNodePath(Node node) {
-    uint32_t i;
-    for(i = 0; i < node->value; i++) {
-        node->visited = false;
-    }
-    printf("%u can be computed in %u operations\n", node->value, node->numOperations);
-    printNodePathRec(node);
+// Return the larger of the values.
+static inline uint32_t max(uint32_t value1, uint32_t value2) {
+    return value1 >= value2? value1 : value2;
 }
 
 int main(int argc, char **argv) {
@@ -117,13 +83,48 @@ int main(int argc, char **argv) {
         printf("Usage: constmult value\n");
     }
     uint32_t targetValue = atoi(argv[1]);
-    Node *nodes = (Node *)calloc(2*targetValue+1, sizeof(Node));
-    Node node = createNode(nodes, 1, NULL, NULL, false);
-    nodes[1] = node;
-    addPowersOfTwo(nodes, node, targetValue); // We get powers of two for free
+    printf("Allocating memory\n");
+    values = (uint32_t *)calloc(2*targetValue+1, sizeof(uint32_t));
+    prevValues = (uint32_t *)calloc(2*targetValue+1, sizeof(uint32_t));
+    nodes = (uint32_t *)calloc(2*targetValue+1, sizeof(uint32_t));
+    visited = (bool *)calloc(2*targetValue+1, sizeof(bool));
+    printf("Done allocating memory\n");
+    createNode(1, 0, targetValue);
+    addPowersOfTwo(1, targetValue); // We get powers of two for free
+    printf("Done adding powers of two\n");
+    uint32_t maxNode = numNodes;
     uint32_t i;
-    for(i = 0; nodes[targetValue] == NULL; i++) {
-        addOneAdderOrSubtractor(nodes, targetValue, i);
+    for(i = 0; nodes[targetValue] == 0; i++) {
+        if(i % 10000 == 0) {
+            printf("Processed %u nodes of %u\n", i, numNodes);
+            fflush(stdout);
+        }
+        if(i == maxNode) {
+            printf("Next level %u - %u\n", maxNode, numNodes);
+            maxNode = numNodes;
+        }
+        uint32_t value1 = values[i];
+        uint32_t j;
+        for(j = 0; j < i; j++) {
+            uint32_t value2 = values[j];
+            uint64_t value = (uint64_t)values[i] + values[j];
+            if(value < 2*targetValue) {
+                if(nodes[value] == 0) {
+                    createNode(value, max(value1, value2), targetValue);
+                    addPowersOfTwo(value, targetValue);
+                }
+            }
+            if(value1 >= value2) {
+                value = value1 - value2;
+            } else {
+                value = value2 - value1;
+            }
+            if(nodes[value] == 0) {
+                createNode(value, max(value1, value2), targetValue);
+                addPowersOfTwo(value, targetValue);
+            }
+        }
     }
-    printNodePath(nodes[targetValue]);
+    printNodePath(targetValue);
+    return 0;
 }
