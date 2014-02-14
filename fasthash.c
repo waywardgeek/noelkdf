b71f16cdb216a4bb1e4a114d05f3cdc01434c4f0
Bill Cox <waywardgeek@gmail.com>
2014-02-14 13:43:50 -0500

    updates to fast hash

--- fasthash.c
+++ fasthash.c
@@ -4,11 +4,12 @@
 #include <string.h>
 #include <getopt.h>
 #include <immintrin.h>
+#include "sha256.h"
 
 int main(int argc, char **argv) {
-    uint32_t parallelism = 2;
     uint64_t memlen = (1u << 31)/sizeof(uint32_t);
-    uint32_t blocklen = 4096/sizeof(uint32_t);
+    uint32_t blocklen = 32768/sizeof(uint32_t);
+    uint32_t parallelism = blocklen/8;
     uint32_t repetitions = 1;
     char c;
     while((c = getopt(argc, argv, "p:m:r:b:")) != -1) {
@@ -31,8 +32,8 @@ int main(int argc, char **argv) {
         }
     }
     uint32_t numblocks = memlen/blocklen;
-    if(parallelism*4 > blocklen) {
-        printf("parallelism must be <= blocksize/16\n");
+    if(parallelism*8 > blocklen) {
+        printf("parallelism must be <= blocksize/32\n");
         return 1;
     }
 
@@ -43,7 +44,8 @@ int main(int argc, char **argv) {
     }
     uint32_t vs = 1;
     uint32_t mask = (blocklen-1) & ~(4*parallelism-1);
-    __m128i vm = _mm_set_epi32(vs, vs, vs, vs);
+    __m128i vm1 = _mm_set_epi32(vs, vs, vs, vs);
+    __m128i vm2 = _mm_set_epi32(vs, vs, vs, vs);
     uint32_t r;
     for(r = 0; r < repetitions; r++) {
         __m128i *tm = (__m128i *)(mem + blocklen);
@@ -53,17 +55,27 @@ int main(int argc, char **argv) {
             uint32_t *fs = mem + fromAddr*blocklen;
             __m128i *fm = (__m128i *)fs;
             uint32_t j;
-            for(j = 0; j < blocklen; j += (parallelism << 2)) {
+            for(j = 0; j < blocklen; j += (parallelism << 3)) {
                 uint32_t *psl = ps + (vs & mask);
                 //uint32_t *psl = ps + j;
+                __m128i *pml = (__m128i *)psl;
                 uint32_t k;
                 for(k = 0; k < parallelism; k++) {
-                    __m128i *pml = (__m128i *)psl;
                     __m128i p = _mm_load_si128(pml++);
                     __m128i f = _mm_load_si128(fm++);
-                    vm = _mm_add_epi32(vm, p);
-                    vm = _mm_xor_si128(vm, f);
-                    _mm_store_si128(tm++, vm);
+                    vm1 = _mm_add_epi32(vm1, p);
+                    vm1 = _mm_xor_si128(vm1, f);
+                    _mm_store_si128(tm++, vm1);
+                    vs = vs * (*fs | 3) + *psl;
+                    vs = vs * (*psl | 3) + *fs;
+                    psl += 4;
+                    fs += 4;
+                    pml = (__m128i *)psl;
+                    p = _mm_load_si128(pml++);
+                    f = _mm_load_si128(fm++);
+                    vm2= _mm_add_epi32(vm2, p);
+                    vm2= _mm_xor_si128(vm2, f);
+                    _mm_store_si128(tm++, vm2);
                     vs = vs * (*fs | 3) + *psl;
                     vs = vs * (*psl | 3) + *fs;
                     psl += 4;
@@ -71,8 +83,15 @@ int main(int argc, char **argv) {
                 }
             }
             ps += blocklen;
+            if((i & 0x3f) == 0) {
+                uint32_t *data = ps - 8;
+                SHA256_Hash_Int(data);
+                vm1 = _mm_load_si128(tm - 2);
+                vm2 = _mm_load_si128(tm - 1);
+            }
             __m128i t = _mm_set_epi32(vs, vs, vs, vs);
-            vm = _mm_xor_si128(vm, t);
+            vm1 = _mm_xor_si128(vm1, t);
+            vm2 = _mm_xor_si128(vm2, t);
         }
     }
     printf("%u\n", mem[rand() % memlen]);
